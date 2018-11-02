536c305e9d5ca97bb697b779a8b812de834b47ec
yinwb <yinwb@guohe.org>
2018-11-02 17:23:32 +0800

    提交算法【1.两数之和】题解

--- two-sum/src/org/yinwenbing/twosum/Solution.java
+++ two-sum/src/org/yinwenbing/twosum/Solution.java
@@ -5,4 +5,25 @@ package org.yinwenbing.twosum;
  * @create 2018-11-02 16:46
  **/
 public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length ==0 ) return null;
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j ++) {
+                if (nums[i] + nums[j] == target && i != j) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 7, 2, 7};
+        int target = 9;
+        Solution solution = new Solution();
+        int[] result = solution.twoSum(nums, target);
+        System.out.println(result);
+    }
 }
